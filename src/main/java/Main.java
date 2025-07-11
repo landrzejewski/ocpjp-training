69e90086bb9d1b9bdab1946080e6f49ec000fa01
Admin <landrzejewski.poczta@gmail.com>
2025-07-11 16:20:11 +0200

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
